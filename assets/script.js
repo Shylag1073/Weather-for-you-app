48fb6fbffa3c1fc148dd46a0acc3da6ac9809549
shylag1073 <shylag1073@gmail.com>
2022-02-03 16:53:19 -0600

    got api to work thanks ben

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var getWeather = function () {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=daily&appid={shyla}")
+    let shyla = '74ba70f345163fd841357d524728306a'
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=daily&appid=74ba70f345163fd841357d524728306a')
 
     console.log("function was called");
 };
