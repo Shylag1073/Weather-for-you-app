972464f27613969495c381e0bbfd6b2e656a4ab5
shylag1073 <shylag1073@gmail.com>
2022-02-03 16:24:47 -0600

    failed api

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
-console.log('u there')
+var getWeather = function () {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=daily&appid={shyla}")
+
+    console.log("function was called");
+};
+
+getWeather();
