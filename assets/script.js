fef07a187e0ace1b7011a855f609fbda5320f8e5
shylag1073 <shylag1073@gmail.com>
2022-02-04 15:51:16 -0600

    adding to html

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var getWeather = function () {
-    let shyla = '74ba70f345163fd841357d524728306a'
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=daily&appid=74ba70f345163fd841357d524728306a')
+    //let shyla = '74ba70f345163fd841357d524728306a'
+    var response = fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=daily&appid=74ba70f345163fd841357d524728306a');
 
-    console.log("function was called");
+    console.log(response);
 };
 
 getWeather();
